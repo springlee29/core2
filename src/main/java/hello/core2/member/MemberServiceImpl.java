a47d6baa6ba08b8c5651e9256b6a0dead5a0b169
bongDev <bongdev29@gmail.com>
2023-04-10 17:19:09 +0900

    spring componentscan 변경및 @Autowired 추가

--- src/main/java/hello/core2/member/MemberServiceImpl.java
+++ src/main/java/hello/core2/member/MemberServiceImpl.java
@@ -1,11 +1,14 @@
 package hello.core2.member;
 
 import hello.core2.discount.FixDiscountPolicy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MemberServiceImpl implements MemberService {
 
     private MemberRepository memberRepository;
-
+    @Autowired
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
